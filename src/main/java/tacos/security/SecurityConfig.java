3254e04375e888e816e3e184b2b2146c1721de14
okapi1985 <andrzej.golian@wp.pl>
2020-02-06 22:02:51 +0100

    DesignAndOrderTacosBrowserTest doesn't work. Login works. Spring security in progress.

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                    .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**").access("permitAll()")
+            .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/design", true)
+            .and()
+                .logout()
+                    .logoutSuccessUrl("/");
+    }
+
+    //Basic
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.authorizeRequests()
+//                .antMatchers("/design", "/orders")
+//                    .hasRole("ROLE_USER")
+//                .antMatchers("/", "/**").permitAll();
+//    }
+
+    //SpEL
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.authorizeRequests()
+//                .antMatchers("/design", "/orders")
+//                    .access("hasRole('ROLE_USER') && " +
+//                        "T(java.util.Calendar).getInstance().get(" +
+//                        "T(java.util.Calendar).DAY_OF_WEEK) == " +
+//                        "T(java.util.Calendar).TUESDAY")
+//                .antMatchers("/", "/**").access("permitAll()");
+//    }
 
     @Bean
     public PasswordEncoder encoder(){
